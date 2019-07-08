1a46484c6b013824c5fec998ada60ee2d5ac7680
Aditya Bagaria <aditya.bagaria@gmail.com>
2019-07-07 22:55:11 -0700

    lab3

--- routes.py
+++ routes.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
-
+import joblib
+import pandas as pd
+import numpy as np
 app=Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	model = joblib.load('regr.pkl')
+	predictpass=[4, 2.5, 3005, 15, 17903.0,0,0,1,0,0,1,0,0]
+	prediction=np.squeeze(model.predict([predictpass]).round(1))
+	return render_template("index.html",prediction=prediction)
 
-if __name__ == "__main__":
-	app.run(debug=True)
+if __name__ == '__main__':
+    model = joblib.load('regr.pkl')
+    app.run(debug=True)
